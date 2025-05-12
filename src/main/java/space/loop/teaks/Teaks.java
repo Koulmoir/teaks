1f3606baca46b2718e391b847ad3c5b0d0955e13
koulmoir <hallonein222000@gmail.com>
2025-05-12 19:24:00 +0200

    Added detection for enchancement and added the config which will overwrite the default

--- src/main/java/space/loop/teaks/Teaks.java
+++ src/main/java/space/loop/teaks/Teaks.java
@@ -1,10 +1,24 @@
 package space.loop.teaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 public class Teaks implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        final Logger LOGGER = LogManager.getLogger("Loopy Teaks");
+        //LOGGER.info(FabricLoader.getInstance().getConfigDir());
+        //checks if the enchancements config file exists
+        File enchancementConfigFile = new File(FabricLoader.getInstance().getConfigDir()+"/enchancement.json");
+        if (enchancementConfigFile.exists()) {
+            LOGGER.info("Enchancement config file found! Configuring...");
+        } else {
+            LOGGER.info("Enchancement config file not found! Please add Enchancements for full immersion");
+        }
     }
 }
