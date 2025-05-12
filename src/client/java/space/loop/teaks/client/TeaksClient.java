ea7592cbec222169c5432c533ea2f3a719d832f1
koulmoir <hallonein222000@gmail.com>
2025-05-12 16:39:26 +0200

    init

---
+++ src/client/java/space/loop/teaks/client/TeaksClient.java
@@ -0,0 +1,11 @@
+package space.loop.teaks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.spongepowered.asm.logging.ILogger;
+
+public class TeaksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/space/loop/teaks/Teaks.java
@@ -0,0 +1,10 @@
+package space.loop.teaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Teaks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
